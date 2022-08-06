f1844539c44a2a8d79f486259eb8eee35081f584
Andrey <teryaev.2004@mail.ru>
2022-08-06 12:26:10 +0300

    fix-bug-done-save

--- src/App.js
+++ src/App.js
@@ -1,59 +1,72 @@
-import React, { useId } from 'react';
-import uuid from 'react-uuid'
+import React, { useId } from "react";
+import uuid from "react-uuid";
 
-import './App.scss';
-import Case from './component/Case/Case';
-import logo from './img/logo.svg';
+import "./App.scss";
+import Case from "./component/Case/Case";
+import logo from "./img/logo.svg";
 
 function App() {
-   const [text, setText] = React.useState('');
-   const [items, setItems] = React.useState(
-      JSON.parse(localStorage.getItem('items')) || []
-   );
-   const id = useId();
+  const [text, setText] = React.useState("");
+  const [items, setItems] = React.useState(
+    JSON.parse(localStorage.getItem("items")) || []
+  );
+  const id = useId();
 
-   const onChangeTextInput = (event) => {
-      setText(event.target.value);
-   };
+  const onChangeTextInput = (event) => {
+    setText(event.target.value);
+  };
 
-   const addItem = (event) => {
-      event.preventDefault();
-      text.length && setItems([...items, {id: uuid(), text, done: false}]);
-      setText('');
-   };
+  const addItem = (event) => {
+    event.preventDefault();
+    text.length && setItems([...items, { id: uuid(), text, done: false }]);
+    setText("");
+  };
 
-   React.useEffect(() => {
-      localStorage.setItem('items', JSON.stringify(items));
-   }, [items]);
+  React.useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
-   const removeItem = (id) => {
-      setItems(prev => prev.filter((item) => item.id !== id));
-   };
+  const removeItem = (id) => {
+    setItems((prev) => prev.filter((item) => item.id !== id));
+  };
 
-   return (
-      <div className="App">
-         <header className="header">
-            <a className="logo" href="#">
-               <img className="logoImg" src={logo} alt="logo"/>
-            </a>
-         </header>
-         <div className="content">
-            <p className="date">{new Date().toLocaleDateString()}</p>
-            <form className="form">
-               <input onChange={onChangeTextInput} value={text} className="textInput" type="text" placeholder="Введите цель"/>
-               <button onClick={addItem} className="addItemBtn">Добавить</button>
-            </form>
-            <div className="contentList">
-               {
-                  items.map((item) => (
-                     <Case key={item.id} text={item.text} remove={() => removeItem(item.id)} done={item.done} id={item.id} items={items}/>
-                  ))
-               }
-            </div>
-         </div>
-         <p className="version">v1.0</p>
+  return (
+    <div className="App">
+      <header className="header">
+        <a className="logo" href="#">
+          <img className="logoImg" src={logo} alt="logo" />
+        </a>
+      </header>
+      <div className="content">
+        <p className="date">{new Date().toLocaleDateString()}</p>
+        <form className="form">
+          <input
+            onChange={onChangeTextInput}
+            value={text}
+            className="textInput"
+            type="text"
+            placeholder="Введите цель"
+          />
+          <button onClick={addItem} className="addItemBtn">
+            Добавить
+          </button>
+        </form>
+        <div className="contentList">
+          {items.map((item) => (
+            <Case
+              key={item.id}
+              text={item.text}
+              remove={() => removeItem(item.id)}
+              done={item.done}
+              id={item.id}
+              items={items}
+            />
+          ))}
+        </div>
       </div>
-   );
+      <p className="version">v1.0</p>
+    </div>
+  );
 }
 
 export default App;
--- src/component/Case/Case.jsx
+++ src/component/Case/Case.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import styles from './Case.module.scss';
-import deleteImg from '../../img/delete.svg';
-import check from '../../img/check.svg';
+import React from "react";
+import styles from "./Case.module.scss";
+import deleteImg from "../../img/delete.svg";
+import check from "../../img/check.svg";
 
 const Case = (props) => {
-   const [done, setDone] = React.useState(false);
+  const [done, setDone] = React.useState(props.done);
 
-   const onDone = (id) => {
-      let local = JSON.parse(localStorage.getItem('items'));
-      setDone(!done);
+  const onDone = (id) => {
+    let local = JSON.parse(localStorage.getItem("items"));
 
-      local.forEach((el, index) => {
-         if(index === id) {
-            el.done = !done;
-         }
-      });
-      localStorage.setItem('items', JSON.stringify(local));
-   };
+    setDone(!done);
 
-   return (
-      <div className={styles.case} style={{backgroundColor: (props.done || done) && '#20AD25', color: (props.done || done) && '#fff'}}>
-         <img onClick={() => onDone(props.id)} src={check} alt="check"/>
-         <span>
-            {props.text}
-         </span>
-         <img onClick={props.remove} src={deleteImg} alt="delete"/>
-      </div>
-   );
-}
+    local.forEach((el) => {
+      if (el.id === id) {
+        el.done = !done;
+      }
+    });
+
+    localStorage.setItem("items", JSON.stringify(local));
+  };
+
+  return (
+    <div
+      className={styles.case}
+      style={{ backgroundColor: done ? "#20AD25" : "#FAFAFA" }}
+    >
+      <img onClick={() => onDone(props.id)} src={check} alt="check" />
+      <span>{props.text}</span>
+      <img onClick={props.remove} src={deleteImg} alt="delete" />
+    </div>
+  );
+};
 
 export default Case;
